153e765f1fe4edd40b9d99f619786134caf50ceb
Martin Gruscher <martingruscher@gmail.com>
2017-01-05 09:05:47 +0100

    first experiments with idt

--- src/boot.s
+++ src/boot.s
@@ -16,6 +16,22 @@ stack_bottom:
 resb 16384              ; 16 KiB
 stack_top:
 
+
+
+section .data
+
+idt_start:
+	times 256 * 8 db 0	; one idt entry is 8 bytes, and we need 256 of them	
+idt_end:
+
+
+idtr:
+	dw idt_end - idt_start - 1
+	dd idt_start
+
+
+cursorPos: dw 0
+
 section .text
 global _start:function (_start.end - _start)
 _start:
@@ -24,12 +40,65 @@ _start:
 	xor edi, edi
 	pop edi
 	mov [ds:edi], word 441h
-        extern kernel_start
-        call kernel_start
-	mov [ds:edi+2], word 442h
+        ;extern kernel_start
+        ;call kernel_start
+	;mov [ds:edi+2], word 442h
+	call idtTest
+	;call idtTest
+	;call idtTest
 
-        cli
+	lidt [idtr]
+	;mov eax, defaultIntHandler
+	;mov [idt_start], ax
+	;mov word [idt_start +2], cs
+	;mov word [idt_start +4], 8e00h
+	;shr eax, 16
+	;mov [idt_start + 6], ax
+	mov eax, defaultIntHandler
+	push eax
+	push word 0
+	mov ebx, [esp]
+	mov eax, [esp + 4]
+	;call installIntHandler
+
+	;int 0
+
+	call idtTest
+
+        ;cli
 .hang:  hlt
         jmp .hang
 .end:
+
+installIntHandler:
+	;pop ebx		; our handler number
+	;pop eax		; our handler address
+	;mov ebx, [sp + 4]
+	;mov eax, [sp + 8]
+	mov ebx, [esp + 4]
+	pusha
+	mov eax, defaultIntHandler
+	mov [idt_start + ebx], ax
+	mov word [idt_start + ebx + 2], cs
+	mov word [idt_start + ebx + 4], 8e00h
+	shr eax, 16
+	mov [idt_start + ebx + 6], ax
+	popa
+	ret
+
+idtTest:
+	pusha
+	mov eax, [cursorPos]
+	mov [ds:0b8000h + eax], word 0448h
+	add word [cursorPos], 2
+	popa
+	ret
+
+defaultIntHandler:
+	pusha
+	mov eax, [cursorPos]
+	mov [ds:0b8000h + eax], word 0449h
+	add word [cursorPos], 2
+	popa
+	iret	
 
