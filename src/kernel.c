999cd34ae1ca37ba6dd3f0509862ff7347fd81dd
Martin Gruscher <martingruscher@gmail.com>
2017-01-06 17:02:49 +0100

    no need to put extra 0 at string literal

--- src/kernel.c
+++ src/kernel.c
@@ -13,7 +13,7 @@ void kernel_start(void) {
 	// write a letter to videomemory
 	//uint16_t *vidmem = (uint16_t*) 0xB8004;
 	//vidmem[0] = 0x449;
-	printk("hello world\0");
+	printk("hello world");
 	//printkc('T');
 
 	//foo();
